fce629804705f284b4a189a470650140b8de9e95
Gabriel Brasileiro <gah.lucas@gmail.com>
2020-03-02 02:46:40 -0300

    Improvements in app layer
    - Addition of redux
    - Implementation of new view models in app layer

--- app/src/main/java/com/universodoandroid/starwarsjetpack/ui/BindingAdapter.kt
+++ app/src/main/java/com/universodoandroid/starwarsjetpack/ui/BindingAdapter.kt
@@ -8,23 +8,22 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BindingAdapter<B : ViewDataBinding> : RecyclerView.Adapter<BindingAdapter<B>.BindingViewHolder<B>>() {
+abstract class BindingAdapter<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int
+) : RecyclerView.Adapter<BindingAdapter<B>.BindingViewHolder<B>>() {
 
-    @LayoutRes
-    abstract fun getLayoutResId(): Int
     abstract fun onBindViewHolder(binding: B, position: Int)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<B> {
-        val view = LayoutInflater.from(parent.context).inflate(getLayoutResId(), parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(layoutResId, parent, false)
         return BindingViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: BindingViewHolder<B>, position: Int) {
-        holder.binding?.let { onBindViewHolder(it, position) }
+        holder.binding?.run { onBindViewHolder(this, position) }
     }
 
     inner class BindingViewHolder<B : ViewDataBinding>(view: View) : RecyclerView.ViewHolder(view) {
         val binding = DataBindingUtil.bind<B>(view)
     }
-
 }
