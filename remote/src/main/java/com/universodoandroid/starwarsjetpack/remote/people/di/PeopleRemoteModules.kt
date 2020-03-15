94782eddb01922b6f7cec47eff458311b3994c0c
Gabriel Brasileiro <gah.lucas@gmail.com>
2020-03-15 16:18:34 -0300

    Segregation of api data source
    - Recover of api data source contract in creation of services

--- remote/src/main/java/com/universodoandroid/starwarsjetpack/remote/people/di/PeopleRemoteModules.kt
+++ remote/src/main/java/com/universodoandroid/starwarsjetpack/remote/people/di/PeopleRemoteModules.kt
@@ -1,22 +1,20 @@
 package com.universodoandroid.starwarsjetpack.remote.people.di
 
 import com.universodoandroid.starwarsjetpack.data.people.datastore.PeopleRemoteData
-import com.universodoandroid.starwarsjetpack.remote.ApiDataSource
 import com.universodoandroid.starwarsjetpack.remote.people.api.PeopleApi
+import com.universodoandroid.starwarsjetpack.remote.people.data.PeopleRemoteDataImpl
+import com.universodoandroid.starwarsjetpack.remote.people.mapper.PersonPageDataMapper
 import com.universodoandroid.starwarsjetpack.remote.people.remote.PeopleRemoteDataSource
 import com.universodoandroid.starwarsjetpack.remote.people.remote.PeopleRemoteDataSourceImpl
-import com.universodoandroid.starwarsjetpack.remote.people.mapper.PersonPageDataMapper
-import com.universodoandroid.starwarsjetpack.remote.people.data.PeopleRemoteDataImpl
+import com.universodoandroid.starwarsjetpack.remote.source.ApiDataSource
 import com.universodoandroid.starwarsjetpack.shared.extensions.getMapper
 import com.universodoandroid.starwarsjetpack.shared.extensions.mapper
 import org.koin.dsl.module
 
-private val remoteModules = module {
+internal fun getPeopleRemoteModules() = module {
     mapper { PersonPageDataMapper() }
 
     factory { get<ApiDataSource>().createService(PeopleApi::class.java) }
     factory<PeopleRemoteDataSource> { PeopleRemoteDataSourceImpl(get()) }
     factory<PeopleRemoteData> { PeopleRemoteDataImpl(get(), getMapper()) }
 }
-
-internal fun getPeopleRemoteModules() = remoteModules
