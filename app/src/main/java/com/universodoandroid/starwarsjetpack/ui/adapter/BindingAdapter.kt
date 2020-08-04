fe44e951b836ab548c65318deb205c114a49a3dd
Gabriel Brasileiro <gah.lucas@gmail.com>
2020-08-03 22:21:02 -0300

    Addition of view parent extension

--- app/src/main/java/com/universodoandroid/starwarsjetpack/ui/adapter/BindingAdapter.kt
+++ app/src/main/java/com/universodoandroid/starwarsjetpack/ui/adapter/BindingAdapter.kt
@@ -1,12 +1,12 @@
 package com.universodoandroid.starwarsjetpack.ui.adapter
 
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
+import com.universodoandroid.starwarsjetpack.extensions.inflate
 
 abstract class BindingAdapter<B : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int
@@ -15,8 +15,7 @@ abstract class BindingAdapter<B : ViewDataBinding>(
     abstract fun onBindViewHolder(binding: B, position: Int)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<B> {
-        val view = LayoutInflater.from(parent.context).inflate(layoutResId, parent, false)
-        return BindingViewHolder(view)
+        return parent.inflate(layoutResId) { view -> BindingViewHolder(view) }
     }
 
     override fun onBindViewHolder(holder: BindingViewHolder<B>, position: Int) {
