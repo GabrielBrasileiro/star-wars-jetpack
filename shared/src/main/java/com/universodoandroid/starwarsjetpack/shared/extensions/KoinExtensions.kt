a697e9e65d375849ef98eaa9a933dac3c35b4cb3
Gabriel Brasileiro <gah.lucas@gmail.com>
2022-01-30 03:37:24 -0300

    Update of libraries

--- shared/src/main/java/com/universodoandroid/starwarsjetpack/shared/extensions/KoinExtensions.kt
+++ shared/src/main/java/com/universodoandroid/starwarsjetpack/shared/extensions/KoinExtensions.kt
@@ -1,26 +1,24 @@
 package com.universodoandroid.starwarsjetpack.shared.extensions
 
 import com.universodoandroid.starwarsjetpack.shared.mapper.Mapper
-import org.koin.core.component.KoinApiExtension
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.koin.core.definition.BeanDefinition
 import org.koin.core.definition.Definition
+import org.koin.core.instance.InstanceFactory
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 
 inline fun <reified I, reified O> Module.mapper(
     noinline definition: Definition<Mapper<I, O>>
-): BeanDefinition<Mapper<I, O>> {
-    return factory(qualifier = named(identifier<I, O>()), definition = definition)
+): Pair<Module, InstanceFactory<Mapper<I, O>>> {
+    return factory(qualifier = named(identifier<I, O>()), definition)
 }
 
 inline fun <reified I, reified O> Scope.getMapper(): Mapper<I, O> {
     return get(named(identifier<I, O>()))
 }
 
-@KoinApiExtension
 inline fun <reified I, reified O> KoinComponent.injectMapper(): Lazy<Mapper<I, O>> {
     return inject(named(identifier<I, O>()))
 }
